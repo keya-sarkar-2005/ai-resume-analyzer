23491754c9662b623a949bd33e7695fcbed31e79
Keya Sarkar <sarkarkeya2005@gmail.com>
2026-06-14 23:11:57 +0530

    Day 5: Connect MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("✅ MongoDB Connected");
+    })
+    .catch((err) => {
+        console.log("❌ MongoDB Error:", err);
+    });
+
+// Test Route
 app.get("/api/message", (req, res) => {
     res.json({
-        message: "Hello from Express Backend 🚀"
+        message: "Hello from Express Backend 🚀",
     });
 });
 
-// Start Server
 const PORT = 5000;
 
 app.listen(PORT, () => {
